a6cb73ad34c97429dcdefedbaac7f39701b6890a
KrzysztofiniOWO <91846431+KrzysztofiniOWO@users.noreply.github.com>
2023-12-04 17:51:47 +0100

    Basic look of app for user

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'Core/index.html')
+
+def call_page(request):
+    return render(request, 'Core/call_page.html')
+
+def go_back_action(request):
+    return render(request, 'Core/index.html')
+
+def menu_page(request):
+    return render(request, 'Core/menu_page.html')
+
+def newsletter_page(request):
+    return render(request, 'Core/newsletter_page.html')
+
+
--- main_apka/urls.py
+++ main_apka/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Core import views as core_views
 
 urlpatterns = [
+    path('', core_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('go_back_action', core_views.go_back_action, name='go_back_action'),
+    path('call_page', core_views.call_page, name='call_page'),
+    path('menu_page', core_views.menu_page, name='menu_page'),
+    path('newsletter_page', core_views.newsletter_page, name='newsletter_page'),
 ]
